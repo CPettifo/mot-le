b9e3da58d5fa26da67d2e9f73a8cb17f6cef4456
Craig Pettifor <craig_petti4@hotmail.com>
2022-12-09 09:20:01 +0100

    added alphabet

--- utilities/de_accent.py
+++ utilities/de_accent.py
@@ -1,9 +1,9 @@
 # This program will find all instances of accented characters in the words and change them into an unaccented version
 
 # initialize all alphabet letters
-
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 # open the fivers.txt file
-
+print (alphabet)
 # read through the file and search for words that aren't composed of entirely alphabet characters. 
 
 # add new "strange" letters to a list to be replaced in the next step below
