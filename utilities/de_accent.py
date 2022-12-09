73ad32dafd5c34db2099a3b77da3f6ebe6ec5deb
Craig Pettifor <craig_petti4@hotmail.com>
2022-12-09 11:04:03 +0100

    removes accents from 'fivers.txt' file successfully

--- utilities/de_accent.py
+++ utilities/de_accent.py
@@ -1,20 +1,84 @@
 # This program will find all instances of accented characters in the words and change them into an unaccented version
-
 # initialize all alphabet letters, and accented letters lists
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+words = []
+de_accented = 0
 
-accented = []
-# open the fivers.txt file
-with open('fivers.txt', 'r') as file:
-# read through the file and search for words that aren't composed of entirely alphabet characters. 
-    for word in file:
+def main(): 
+    write_list_from_txt()
+    check_list()
+
+
+def write_list_from_txt():
+    global alphabet
+    global words
+    counter = 0
+    # open the fivers.txt file
+    with open('fivers.txt', 'r') as file:
+    # read through the file and search for words that aren't composed of entirely alphabet characters. 
+        for word in file:
+            print (word)
+            counter = 0
+            for letter in word:
+                if letter not in alphabet:
+                    print(letter)
+                    word = word.replace(letter, (replace_char(letter)))
+                    counter += 1
+                else:
+                    counter += 1 
+            words.append(word)
+
+
+
+# replaces the accented characters with their base letter
+def replace_char(char):
+    # initialize lists with the accented characters
+    print("got here")
+    global de_accented
+    a = ['â', 'æ', 'á', 'à']
+    e = ['é', 'è', 'ê','ë']
+    i = ['ï', 'î']
+    o = ['ô', 'œ', 'ō']
+    u = ['û', 'ü']
+    c = ['ç', 'č']
+    n = ['ñ']
+    if char in a:
+        char = 'a'
+    elif char in e:
+        char = 'e'
+    elif char in i:
+        char = 'i'
+    elif char in o:
+        char = 'o'
+    elif char in u:
+        char = 'u'
+    elif char in c:
+        char = 'u'
+    elif char in n:
+        char = 'n'
+    else:
+        print("character not found")
+    de_accented += 1
+    return char
+
+# this function checks the list for any characters that aren't in alphabet
+def check_list():
+    # empty the un_accented .txt file 
+    f = open ("un_accented.txt", "w")
+    f.write('')
+    f.close()
+    print("got here")
+    global alphabet
+    global words
+    global de_accented
+    for word in words:
         for letter in word:
             if letter not in alphabet:
-                temp_letter = letter
-            if letter == 'ˡ' or letter == 'ᵉ' or letter == 'ˢ':
-                print (word)
-# add new "strange" letters to a list to be replaced in the next step below
-                if temp_letter not in accented:
-                    accented.append(temp_letter)
+                print("whoops")
+                print(word)
+        f = open("un_accented.txt", "a")
+        f.write(word)
+        f.close()
+    print(f" {de_accented} accented letters removed")
 
-print(accented)
+main()
